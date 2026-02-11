6740f729c29980500943fe2955086f6e53b48eec
Igor Rumiantcev <i.rumiantcev@ddl.com>
2026-02-11 14:44:26 +0100

    Update all site links to use the ".com" domain instead of ".pro"

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const scaleFactor = 4;
 export const dialogueData = {
     greeting: `Hi👋
     My name is Igor and I am a dedicated Frontend Developer with a strong passion for Single Page Applications (SPAs) using React and Mobile App development with React Native.
-    This is my site in mini-game format. If you don't want to play, follow this <a href="https://t.rumiantcev.pro" target="_blank">link</a>.
+    This is my site in mini-game format. If you don't want to play, follow this <a href="https://t.rumiantcev.com" target="_blank">link</a>.
     `,
 
     pc: `This is my PC. These days, I primarily work in JavaScript/TypeScript. I've completed several projects using these languages and also have a fondness for PHP. Over the last 6 years, I've honed my skills as a FullStack Developer, with a focus on frontend (70%) and backend (30%) development. Regardless of the language, I simply enjoy programming.`,
@@ -16,7 +16,7 @@ export const dialogueData = {
 
     bed: `It's not just a bed; it's where inspiration meets comfort, nurturing my passion for innovation and shaping my dreams into reality.`,
 
-    resume: `This is my CV. I am very proud of my experiences and the projects I've worked on. Please <a href="https://t.rumiantcev.pro" target="_blank">contact me</a>, and I will send it to you.`,
+    resume: `This is my CV. I am very proud of my experiences and the projects I've worked on. Please <a href="https://t.rumiantcev.com" target="_blank">contact me</a>, and I will send it to you.`,
 
     projects: `I specialize as a Frontend Developer, deeply committed to crafting Single Page Applications (SPAs) with React and developing mobile apps using React Native. 
     Additionally, I excel in the dynamic realm of 3D computer graphics and web animation, employing tools such as Three.js, GreenSock, Kaboom.js to realize imaginative concepts.
@@ -25,5 +25,5 @@ export const dialogueData = {
 
     library: `There are a lot of programming books on my shelves. Programming isn't the only thing that motivates me. Fiction also holds a significant place on my bookshelf. Could you suggest something thought-provoking for me to read?`,
 
-    exit: `Thank you for visiting my website. If you want, you can follow my site using this <a href="https://t.rumiantcev.pro" target="_blank">link</a>.`,
+    exit: `Thank you for visiting my website. If you want, you can follow my site using this <a href="https://t.rumiantcev.com" target="_blank">link</a>.`,
 };
